b182015c5e3e337ff3b00b62e5c42acbbf3997ec
Ainst77 <61785626+Ainst77@users.noreply.github.com>
2020-10-03 18:24:49 +0200

    Add files via upload

---
+++ Question 2/errorMessage.js
@@ -0,0 +1,5 @@
+
+
+function errorMessage(message = "an error accured"){
+    return `<div class="errorContainer">${message}</div>`;
+}  
---
+++ question-1.js
@@ -0,0 +1,15 @@
+//Question 1
+
+
+
+//function division(a, b) {
+//    return a % b;
+//}
+ 
+    const division = (a, b) => a / b;
+
+    const svar = division (15, 3);
+    
+
+
+   
